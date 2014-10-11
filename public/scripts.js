e687a8d605fb829a65f111ed6251360396c9b9a5
optikfluffel <optik@fluffel.io>
2014-10-11 21:04:31 +0200

    update game content after answering

--- public/scripts.js
+++ public/scripts.js
@@ -21,6 +21,51 @@ $(document).ready(function () {
   enableFancyLoginAndRegisterNavbarForm('login');
   enableFancyLoginAndRegisterNavbarForm('register');
 
+  function initializeNewGameRound(newGame) {
+    // deconstruct newGame
+    var newGameId = newGame.id;
+    var newQuestion = newGame.question;
+    var newChoices = newGame.products;
+
+    // get elements
+    var $wrapper = $('#fiftycent');
+    var $question = $('.question.row').children('h1');
+    var $products = $('.products.row');
+    var $choices = $products.children('.game-link');
+
+    // update question
+    var $smallQuestionExtension = $('<small />').text('auf 100g');
+    $question.text(newQuestion).append($smallQuestionExtension);
+
+    // update choices
+    $choices.each(function(index) {
+      var newChoice = $.parseJSON(newChoices[index]);
+      var newChoiceId = newChoice.id;
+
+      $(this).data('game', newGameId);
+      $(this).data('id', newChoiceId);
+
+      $image = $(this).children('img');
+      $image.attr('src', newChoice.imgurl)
+            .attr('alt', newChoice.name);
+
+      $name = $(this).children('h3');
+      $name.text(newChoice.name);
+    })
+  }
+
+  function handlePlayAPIAnswer(data) {
+    var answer = $.parseJSON(data);
+
+    // TODO: show nicer flash messages
+    if (answer.correct) {
+      alert('YAY! You are totally right!');
+    } else {
+      alert('Oh no, I am sorry to correct you but it\'s the other way around.');
+    }
+    $.getJSON('/json/play', initializeNewGameRound);
+  }
+
   function enableEvenFancierGameLinksSoYouCanActuallyPlayNow($link) {
     var gameId = $link.data('game');
     var id = $link.data('id');
@@ -32,12 +77,10 @@ $(document).ready(function () {
         guess: id
       };
 
-      $.post( '/play', answer).done(function(result) {
-        alert( "Data Loaded: " + result );
-        console.log( "Data Loaded: " + result );
-      });
+      // TODO: Spinner and deactivation etc
+
+      $.post('/play', answer).done(handlePlayAPIAnswer);
     });
-    console.log(id);
   }
 
   $('.game-link').each(function() {
